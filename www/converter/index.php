2717c126c2362057ef96fa56558dad07949c8e54
matthewneil <matthewneil06@gmail.com>
2019-10-02 15:24:47 +1300

    Updated application, now lets the user choose a time rather than using the computers set time

--- www/converter/index.php
+++ www/converter/index.php
@@ -1,52 +1,92 @@
 <html>
-<body>
+  <body>
 
-<?php
+    <?php
 
-if(strcmp($_GET['timezone'], "nzt") == 0){
-  $datetime = new DateTime();
-  $nz_time = new DateTimeZone('Pacific/Auckland');
-  $datetime->setTimezone($nz_time);
-  echo $datetime->format('H:i:s'); 
-}
-if(strcmp($_GET['timezone'], "est") == 0){
-$datetime = new DateTime();
-$est_time = new DateTimeZone('America/New_York');
-$datetime->setTimezone($est_time);
-echo $datetime->format('H:i:s'); 
-}
-if(strcmp($_GET['timezone'], "jst") == 0){
-$datetime = new DateTime();
-$jst_time = new DateTimeZone('Asia/Tokyo');
-$datetime->setTimezone($jst_time);
-echo $datetime->format('H:i:s');
-}
-if(strcmp($_GET['timezone'], "msk") == 0){
-$datetime = new DateTime();
-$msk_time = new DateTimeZone('Europe/Moscow');
-$datetime->setTimezone($msk_time);
-echo $datetime->format('H:i:s');
-}
-if(strcmp($_GET['timezone'], "sgt") == 0){
-$datetime = new DateTime();
-$sgt_time = new DateTimeZone('Asia/Singapore');
-$datetime->setTimezone($sgt_time);
-echo $datetime->format('H:i:s');
-}
-if(strcmp($_GET['timezone'], "aest") == 0){
-$datetime = new DateTime();
-$aest_time = new DateTimeZone('Australia/Brisbane');
-$datetime->setTimezone($aest_time);
-echo $datetime->format('H:i:s');
-}
-if(strcmp($_GET['timezone'], "clst") == 0){
-$datetime = new DateTime();
-$clst_time = new DateTimeZone('Antarctica/Palmer');
-$datetime->setTimezone($clst_time);
-echo $datetime->format('H:i:s');
-}
-?>
+       $time = $_GET['time'];
+       
+       if(strcmp($_GET['am/pm'], "am") == 0){
+       	if(strcmp($_GET['time'], "12:00") == 0){
+       	  $time = "0:00";
+        }
+       }
+       if(strcmp ($_GET['am/pm'], "pm") == 0){
+       	if(strcmp($_GET['time'], "1:00") == 0){
+       	  $time = "13:00";
+        }
+        if(strcmp($_GET['time'], "2:00") == 0){
+          $time = "14:00";
+        }
+        if(strcmp($_GET['time'], "3:00") == 0){
+          $time = "15:00";
+        }
+        if(strcmp($_GET['time'], "4:00") == 0){
+          $time = "16:00";
+        }
+        if(strcmp($_GET['time'], "5:00") == 0){
+         $time = "17:00";
+        }
+        if(strcmp($_GET['time'], "6:00") == 0){
+         $time = "18:00";
+        }
+        if(strcmp($_GET['time'], "7:00") == 0){
+         $time = "19:00";
+        }
+        if(strcmp($_GET['time'], "8:00") == 0){
+         $time = "20:00";
+        }
+        if(strcmp($_GET['time'], "9:00") == 0){
+         $time = "21:00";
+        }
+        if(strcmp($_GET['time'], "10:00") == 0){
+         $time = "22:00";
+        }
+        if(strcmp($_GET['time'], "11:00") == 0){
+         $time = "23:00";
+        }
+       
+       }
 
-</body>
+       date_default_timezone_set('Pacific/Auckland');
+       $datetime = new DateTime($time);
+       
+       if(strcmp($_GET['timezone'], "nzt") == 0){
+        $nz_time = new DateTimeZone('Pacific/Auckland');
+        $datetime->setTimezone($nz_time);
+        echo $datetime->format('H:i:s'); 
+       }
+       if(strcmp($_GET['timezone'], "est") == 0){
+        $est_time = new DateTimeZone('America/New_York');
+        $datetime->setTimezone($est_time);
+        echo $datetime->format('H:i:s'); 
+       }
+       if(strcmp($_GET['timezone'], "jst") == 0){
+        $jst_time = new DateTimeZone('Asia/Tokyo');
+        $datetime->setTimezone($jst_time);
+        echo $datetime->format('H:i:s');
+       }
+       if(strcmp($_GET['timezone'], "msk") == 0){
+        $msk_time = new DateTimeZone('Europe/Moscow');
+        $datetime->setTimezone($msk_time);
+        echo $datetime->format('H:i:s');
+       }
+       if(strcmp($_GET['timezone'], "sgt") == 0){
+        $sgt_time = new DateTimeZone('Asia/Singapore');
+        $datetime->setTimezone($sgt_time);
+        echo $datetime->format('H:i:s');
+       }
+       if(strcmp($_GET['timezone'], "aest") == 0){
+        $aest_time = new DateTimeZone('Australia/Brisbane');
+        $datetime->setTimezone($aest_time);
+        echo $datetime->format('H:i:s');
+       }
+       if(strcmp($_GET['timezone'], "clst") == 0){
+        $clst_time = new DateTimeZone('Antarctica/Palmer');
+        $datetime->setTimezone($clst_time);
+        echo $datetime->format('H:i:s');
+       }
+      ?>
+
+  </body>
 </html>
 
--- www/index.php
+++ www/index.php
@@ -1,118 +1,140 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<h1 id="demo"> World Timezone Converter </h1>
-</head>
+  <head>
+    <h1 id="demo"> World Timezone Converter </h1>
+  </head>
 
-<?php
-   $ip = "http://ec2-52-200-184-187.compute-1.amazonaws.com";
-?>
+  <?php
+     $ip = "http://ec2-52-200-184-187.compute-1.amazonaws.com";
+     ?>
 
 
-<body>
-  <p> <?php $tz = "?timezone=nzt";
-         $time = file_get_contents($ip.$tz);?> </p>
-<p>Current New Zealand time: <?php echo "$time"; ?></p>
+  <body>
+    <p> <?php /* $tz = "?timezone=nzt";
+              $time = file_get_contents($ip.$tz);?> </p>
+    <p>Current New Zealand time: <?php echo "$time"; */ ?></p>
 
-<p id="output"></p>
-<?php
-session_start();
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['Convert'])){
-func();
-}
+    <p id="output"></p>
+    <?php
+       session_start();
+       if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['Convert'])){
+       func();
+       }
 
-   function func(){
-   $ip = "http://ec2-52-200-184-187.compute-1.amazonaws.com";
-$timezone = $_POST['timezone'];
-   $db_host = 'tzdatabase.cjzs748frcfu.us-east-1.rds.amazonaws.com';
-$db_name = 'tzdatabase';
-$db_user = 'admin';
-$db_passwd = 'Quack1nce4^';
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-   if($timezone == "EST"){
-   $tze = "?timezone=est";
-$est = file_get_contents($ip.$tze);
-$_SESSION['times'] = "<p> Eastern Standard Time: $est </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'EST' WHERE userID='User'");
-} else if($timezone == "JST"){
-$tzj = "?timezone=jst";
-$jst = file_get_contents($ip.$tzj);
-$_SESSION['times'] = "<p> Japan Standard Time: $jst </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'JST' WHERE userID='User'");
-} else if($timezone == "MSK"){
-$tzm = "?timezone=msk";
-$msk = file_get_contents($ip.$tzm);
-$_SESSION['times'] = "<p> Moscow Time: $msk </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'MSK' WHERE userID='User'");
-} else if($timezone == "SGT"){
-$tzs = "?timezone=sgt";
-$sgt = file_get_contents($ip.$tzs);
-$_SESSION['times'] = "<p> Singapore Time: $sgt </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'SGT' WHERE userID='User'");
-} else if($timezone == "AEST"){
-$tza = "?timezone=aest";
-$aest = file_get_contents($ip.$tza);
-$_SESSION['times'] = "<p> Australian Eastern Standard Time: $aest </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'AEST' WHERE userID='User'");
-} else if($timezone == "CLST"){
-$tzc = "?timezone=clst";
-$clst = file_get_contents($ip.$tzc);
-$_SESSION['times'] = "<p> Antarctica (Palmer Station) Time: $clst </p>";
-$q = $pdo->query("UPDATE timezones SET defaultzone = 'CLST' WHERE userID='User'");
-}
+       function func(){
+       $ip = "http://ec2-52-200-184-187.compute-1.amazonaws.com";
+       $timezone = $_POST['timezone'];
+       $time = $_POST['time'];
+       $ampm = $_POST['am/pm'];
+       $db_host = 'tzdatabase.cjzs748frcfu.us-east-1.rds.amazonaws.com';
+       $db_name = 'tzdatabase';
+       $db_user = 'admin';
+       $db_passwd = 'Quack1nce4^';
+       $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+       $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+       if($timezone == "EST"){
+       $tze = "?timezone=est";
+       $est = file_get_contents($ip.$tze);
+       $_SESSION['times'] = "<p> Eastern Standard Time: $est </p>";
+       $q = $pdo->query("UPDATE timezones SET defaultzone = 'EST' WHERE userID='User'");
+    } else if($timezone == "JST"){
+    $tzj = "?timezone=jst";
+    $jst = file_get_contents($ip.$tzj);
+    $_SESSION['times'] = "<p> Japan Standard Time: $jst </p>";
+    $q = $pdo->query("UPDATE timezones SET defaultzone = 'JST' WHERE userID='User'");
+    } else if($timezone == "MSK"){
+    $tzm = "?timezone=msk";
+    $msk = file_get_contents($ip.$tzm);
+    $_SESSION['times'] = "<p> Moscow Time: $msk </p>";
+    $q = $pdo->query("UPDATE timezones SET defaultzone = 'MSK' WHERE userID='User'");
+    } else if($timezone == "SGT"){
+    $tzs = "?timezone=sgt";
+    $sgt = file_get_contents($ip.$tzs);
+    $_SESSION['times'] = "<p> Singapore Time: $sgt </p>";
+    $q = $pdo->query("UPDATE timezones SET defaultzone = 'SGT' WHERE userID='User'");
+    } else if($timezone == "AEST"){
+    $tza = "?timezone=aest";
+    $aest = file_get_contents($ip.$tza);
+    $_SESSION['times'] = "<p> Australian Eastern Standard Time: $aest </p>";
+    $q = $pdo->query("UPDATE timezones SET defaultzone = 'AEST' WHERE userID='User'");
+    } else if($timezone == "CLST"){
+    $tzc = "?timezone=clst";
+    $clst = file_get_contents($ip.$tzc);
+    $_SESSION['times'] = "<p> Antarctica (Palmer Station) Time: $clst </p>";
+    $q = $pdo->query("UPDATE timezones SET defaultzone = 'CLST' WHERE userID='User'");
+    }
 
-}
-?>
+    }
+    ?>
 
-<?php
+    <?php
 
-$db_host = 'tzdatabase.cjzs748frcfu.us-east-1.rds.amazonaws.com';
-$db_name = 'tzdatabase';
-$db_user = 'admin';
-$db_passwd = 'Quack1nce4^';
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-$q = $pdo->query("SELECT * FROM timezones");
-while($row = $q->fetch()){
-$default = $row["defaultzone"];
+       $db_host = 'tzdatabase.cjzs748frcfu.us-east-1.rds.amazonaws.com';
+       $db_name = 'tzdatabase';
+       $db_user = 'admin';
+       $db_passwd = 'Quack1nce4^';
+       $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+       $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+       $q = $pdo->query("SELECT * FROM timezones");
+    while($row = $q->fetch()){
+    $default = $row["defaultzone"];
 
-if($default == $timezone){
+    if($default == $timezone){
 
-header("refresh: 0;");
-}
-}
-$_SESSION['time'] = $default;
-?>
-<?php
-function getSelectSession($val, $val2)
-{
-if ($_SESSION[$val] == $val2) {
-echo "selected";
-}
-}
-?>
+    header("refresh: 0;");
+    }
+    }
+    $_SESSION['time'] = $default;
+    ?>
+    <?php
+       function getSelectSession($val, $val2)
+       {
+       if ($_SESSION[$val] == $val2) {
+       echo "selected";
+       }
+       }
+       ?>
 
-<form action="index.php" method="post">
+    <form action="index.php" method="post">
 
-<p> Convert from NZT to:
-<select name="timezone">
-<option value="EST" <?php getSelectSession("time", "EST"); ?>>Eastern Standard Time</option>
-<option value="JST" <?php getSelectSession("time", "JST"); ?>>Japan Standard Time</option>
-<option value="MSK" <?php getSelectSession("time", "MSK"); ?>>Moscow Time</option>
-<option value="SGT" <?php getSelectSession("time", "SGT"); ?>>Singapore Time</option>
-<option value="AEST"<?php getSelectSession("time", "AEST"); ?>>Australian Eastern Standard Time</option>
-<option value="CLST"<?php getSelectSession("time", "CLST"); ?>>Antarctica (Palmer Station) Time</option>
-</select>
+      <p> Choose a time to convert:
+        <select name="time">
+          <option value="1:00">1:00</option>
+          <option value="2:00">2:00</option>
+          <option value="3:00">3:00</option>
+          <option value="4:00">4:00</option>
+          <option value="5:00">5:00</option>
+          <option value="6:00">6:00</option>
+          <option value="7:00">7:00</option>
+          <option value="8:00">8:00</option>
+          <option value="9:00">9:00</option>
+          <option value="10:00">10:00</option>
+          <option value="11:00">11:00</option>
+          <option value="12:00">12:00</option>
+        </select>
+        <select name="am/pm">
+          <option value="am">AM</option>
+          <option value="pm">PM</option>
+        </select>
 
-<input type="submit" name="Convert" value="Convert"></input>
+      <p> Convert from NZT to:
+        <select name="timezone">
+          <option value="EST" <?php getSelectSession("time", "EST"); ?>>Eastern Standard Time</option>
+          <option value="JST" <?php getSelectSession("time", "JST"); ?>>Japan Standard Time</option>
+          <option value="MSK" <?php getSelectSession("time", "MSK"); ?>>Moscow Time</option>
+          <option value="SGT" <?php getSelectSession("time", "SGT"); ?>>Singapore Time</option>
+          <option value="AEST"<?php getSelectSession("time", "AEST"); ?>>Australian Eastern Standard Time</option>
+          <option value="CLST"<?php getSelectSession("time", "CLST"); ?>>Antarctica (Palmer Station) Time</option>
+        </select>
 
-</form>
+        <input type="submit" name="Convert" value="Convert"></input>
 
-<?php
-echo $_SESSION['times'];
-$_SESSION['times'] = "";
-?>
+    </form>
 
-</body>
+    <?php
+       echo $_SESSION['times'];
+       $_SESSION['times'] = "";
+       ?>
+
+  </body>
 </html>
